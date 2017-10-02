7642c5fe7155d794e38abb6c519c6a73414ced2a
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2017-10-01 21:31:58 -0400

    UPDATES TO TEST

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      flash[:success] = "Welcome to the Daniel Kraken Estates!"
+      flash[:success] = "Welcome to the Daniel Kraken Estates!!!"
       redirect_to @user
     else
       render 'new'
