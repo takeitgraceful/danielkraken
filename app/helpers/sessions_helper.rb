7fb2b7fe67e56eeae852b143684c0dd69cc1a548
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2017-10-15 00:20:32 -0400

    troubleshooting users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,18 +26,7 @@ def current_user
   end
 end
 
-# Returns the user corresponding to the remember token cookie.
-def current_user
-  if (user_id = session[:user_id])
-    @current_user ||= User.find_by(id: user_id)
-  elsif (user_id = cookies.signed[:user_id])
-    user = User.find_by(id: user_id)
-    if user && user.authenticated?(cookies[:remember_token])
-      log_in user
-      @current_user = user
-    end
-  end
-end
+
 
 # Forgets a persistent session.
 def forget(user)
