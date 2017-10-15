1b4d931b94e47a6b6ec08299b23ed17093f822c9
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2017-10-14 23:35:42 -0400

    Updates to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-User.update(name:  "Takeitgraceful",
-             email: "takeitgraceful@gmail.com",
-             password:              "c3H-i-eof",
-             password_confirmation: "c3H-i-eof",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
+User.update(name:  "Jacob",
+             email: "mcmasterbigumjacob@gmail.com",
+             admin: true)
