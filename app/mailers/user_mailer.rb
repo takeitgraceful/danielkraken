73c0bac532d3d5f135d2abbd17d00f6f06d8550d
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2017-10-14 21:48:55 -0400

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,11 @@ class UserMailer < ApplicationMailer
 
     mail to: "to@example.org"
   end
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+  end
+
+
 end
