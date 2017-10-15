a98514f817be8f8ed9cf2fecb1bdf35f792f4b6e
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2017-10-14 23:26:56 -0400

    Updates to seed

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -59,5 +59,4 @@ class PasswordResetsController < ApplicationController
         redirect_to new_password_reset_url
       end
     end
-    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(name:  "Takeitgraceful",
+User.update!(name:  "Takeitgraceful",
              email: "takeitgraceful@gmail.com",
              password:              "c3H-i-eof",
              password_confirmation: "c3H-i-eof",
-             admin: true
+             admin: true,
              activated: true,
              activated_at: Time.zone.now)
