67a198430c0533c187272e55b7c927732c65cba6
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2017-11-17 23:16:30 -0500

    Troubleshooting Forms and Login Redirect

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
 
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to signup_url # halts request cycle
+    end
+  end
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
