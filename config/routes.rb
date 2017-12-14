8bba6566550a6e7459d57472ee5a726f006640ae
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.home>
2017-12-14 14:33:45 -0500

    Pocketwatch 2

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get  '/spirits',    to: 'users#spirits'
   get  '/story',    to: 'users#story'
   get  '/prologue',    to: 'users#prologue'
+  get  '/pocketwatch',    to: 'users#pocketwatch'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
