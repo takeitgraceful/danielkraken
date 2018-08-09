4ba6ea812bf79ade7868194c54540acf8039e336
Jacob McMaster-Bigum <jacobmcmaster-bigum@Jacobs-MBP.fios-router.home>
2018-08-09 00:58:35 -0400

    Adding login requirements to each chapter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  before_action :require_login, only: :story
+  before_action :require_login, only: [:story, :bolide_rodeo, :leviathan_bread, :daniel_cries,
+  :bolide_rodeo, :saltlick_jukebox, :boardroom, :sailing_to_mount_oligarch, :merlot_bot_hatches_a_plan,
+:wild_white_automoton, :grim_persistence, :ingot_exchange, :salt_peter_online, :the_ascent,
+:an_exquisite_vintage, :tad_excessive, :tiger_farm, :taste_of_betrayal, :world_map]
 end
